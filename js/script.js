b0cddddc04aa81fff2ba7bdc7e47f5395ee14a47
Ivy Kabere <kabereivy@gmail.com>
2022-02-06 15:03:08 +0300

    ft:object - feature - add object correctAnswers

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const corrrectAnswers = {
+  qstn1: "()[].++",
+  qstn2: "NO",
+  qstn3: "Properties",
+  qstn4: "Place where code is executed using execution context",
+  qstn5: "const arr={1,2,3,4}",
+  qstn6: "Decrements length by 1",
+};
+
+let selectedAnswers = {};
